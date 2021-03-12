3d990c5ec5aed4aa850e4cfed8b0d66c9f81ef8d
aturi <angelaturi99@gmail.com>
2021-03-12 13:35:24 -0500

    Initial commit

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,32 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+    
+
+    def initialize(value, parent = nil, children = [])
+        @parent = parent
+        @children = children
+        @value = value 
+    end 
+
+    #new node is the new parent 
+    def parent=(new_node)
+        if @parent == nil 
+            @parent = new_node 
+            @parent.children << self 
+        elsif new_node == nil
+            @parent = nil 
+        else 
+            @parent.children.delete(self)
+            @parent = new_node
+            @parent.children << self 
+        end 
+    end 
+
+    def add_child(child_node)
+        child_node.parent = self 
+    end 
+
+
+
 end
