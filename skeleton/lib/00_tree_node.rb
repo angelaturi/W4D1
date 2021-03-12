661c9849e044657bb3db2e7fa4be3c53828c5367
aturi <angelaturi99@gmail.com>
2021-03-12 14:21:35 -0500

    Update PolyTreeNode

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -27,6 +27,28 @@ class PolyTreeNode
         child_node.parent = self 
     end 
 
+    def remove_child(child_node)
+        child_node.parent = nil
+    end
 
+    def dfs(target_value)
+        return self if self.value == target_value
+
+        self.children.each do |child|
+            res = child.dfs(target_value)
+            return res if res != nil
+        end
+        nil
+    end
+
+    def bfs(target_value)
+
+        @queue = [self]
+        until @queue.empty?
+            current = @queue.shift
+            return current if current.value == target_value
+            @queue += current.children
+        end
+    end
 
 end
